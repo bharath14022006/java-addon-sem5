ea4c2539fcd6b56a4189972923c19fe9b683934f
bharath14022006 <2322jc10@kgcas.com>
2025-06-05 12:05:03 +0530

    Update even.java

--- even.java
+++ even.java
@@ -1,10 +1,13 @@
 import java.util.*;
-class even {
+class Main {
     public static void main(String[] args) {
-        System.out.println("enter the number you need");
+        System.out.println("enter the number start no");
         Scanner even=new Scanner(System.in);
         int a=even.nextInt();
-        for(int i=1;i<=a;i++){
+          System.out.println("enter the number end no");
+        Scanner evenn=new Scanner(System.in);
+        int b=evenn.nextInt();
+        for(int i=a;i<=b;i++){
             if(i%2 ==0)
         System.out.println("the even number is"+i);
             
