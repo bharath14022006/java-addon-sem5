a87fe54e9f49271e74fd24b742f59f44d64d48dd
Bharath.A <2322jc10@kgcas.com>
2025-06-16 14:16:30 +0530

    Create  Student.java

---
+++ day7/Student.java
@@ -0,0 +1,11 @@
+create table Student(More actions
+Roll_number int,
+Student_name varchar(10),
+Phn_number int
+);
+insert into Student (Roll_number,Student_name,Phn_number)
+values(0123,'aaaa',1234567890);More actions
+values(2345,'bbbb',0987654321);
+values(0356,'cccc',2466789047);
+values(0443,'dddd',7654321815);
+values(0578,'eeee',7890653411);
